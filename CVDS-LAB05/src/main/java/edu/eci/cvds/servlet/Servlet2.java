a06fe789b644796edb8a25969467d43438a68ec5
unknown <jordy.bautista@labinfo.is.escuelaing.edu.co>
2023-03-16 13:00:59 -0500

    Excepciones de servlet2

--- CVDS-LAB05/src/main/java/edu/eci/cvds/servlet/Servlet2.java
+++ CVDS-LAB05/src/main/java/edu/eci/cvds/servlet/Servlet2.java
@@ -10,17 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(urlPatterns = "/helloServlet")
+@WebServlet(urlPatterns = "/mundoTaco")
 public class Servlet2 extends HttpServlet{
     static final long serialVersionUID = 35L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Writer responseWriter = resp.getWriter();
-        Optional<String> optName = Optional.ofNullable(req.getParameter("name"));
-        String name = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
-        resp.setStatus(HttpServletResponse.SC_OK);
-        responseWriter.write("Hello" + name + "!");
-        responseWriter.flush();
+        try {
+            Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+            String name = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+            resp.setStatus(HttpServletResponse.SC_OK);
+            responseWriter.write("Hello" + name + "!");
+            responseWriter.flush();
+            resp.setContentType("text/html");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
