31e4a61aea2cf04c3d67f0c4b3019fffa0c345b4
jordy santiago bautista sepulveda <jordy.sbautista@gmail.com>
2023-03-17 08:23:46 -0500

    Pagina web

--- CVDS-LAB05/src/main/java/edu/eci/cvds/servlet/Servlet2.java
+++ CVDS-LAB05/src/main/java/edu/eci/cvds/servlet/Servlet2.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.annotation.WebServlet;
@@ -15,42 +13,77 @@ import javax.servlet.http.HttpServletResponse;
 
 import edu.eci.cvds.servlet.model.Todo;
 
-
 @WebServlet(urlPatterns = "/mundoTaco")
-public class Servlet2 extends HttpServlet{
+public class Servlet2 extends HttpServlet {
     static final long serialVersionUID = 35L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Writer responseWriter = resp.getWriter();
         try {
-            int idNumber = Integer.valueOf( req.getParameter("id"));
+            int idNumber = Integer.valueOf(req.getParameter("id"));
             Todo todo = Service.getTodo(idNumber);
-            ArrayList<Todo> todosList = new ArrayList<Todo>() ;
+            ArrayList<Todo> todosList = new ArrayList<Todo>();
             todosList.add(todo);
-            resp.setStatus(HttpServletResponse.SC_OK); 
+            resp.setStatus(HttpServletResponse.SC_OK);
             String mensajeTodos = Service.todosToHTMLTable(todosList);
             responseWriter.write(mensajeTodos);
             responseWriter.flush();
             resp.setContentType("text/html");
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             if (e instanceof FileNotFoundException) {
-                resp.setStatus(HttpServletResponse.SC_NOT_FOUND); 
-                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: " + e.getLocalizedMessage() + "\n Menssaje: id no encontrado");
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: id no encontrado");
+            } else if (e instanceof NumberFormatException) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: Valor no valido");
+            } else if (e instanceof MalformedURLException) {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: " + e.getMessage());
+            } else {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: " + e.getMessage());
             }
-            else if (e instanceof NumberFormatException) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); 
-                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: " + e.getLocalizedMessage() + "\n Menssaje: Valor no valido" );
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
+        try {
+            int idNumber = Integer.valueOf(req.getParameter("id"));
+            Todo todo = Service.getTodo(idNumber);
+            ArrayList<Todo> todosList = new ArrayList<Todo>();
+            todosList.add(todo);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            String mensajeTodos = Service.todosToHTMLTable(todosList);
+            responseWriter.write(mensajeTodos);
+            responseWriter.flush();
+            resp.setContentType("text/html");
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (e instanceof FileNotFoundException) {
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: id no encontrado");
+            } else if (e instanceof NumberFormatException) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: Valor no valido");
+            } else if (e instanceof MalformedURLException) {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: " + e.getMessage());
+            } else {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: "
+                        + e.getLocalizedMessage() + "\n Menssaje: " + e.getMessage());
             }
-            else if (e instanceof MalformedURLException) {
-                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); 
-                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: " + e.getLocalizedMessage() + "\n Menssaje: " + e.getMessage());
-            }
-            else {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); 
-                responseWriter.write("Numero de error: " + resp.getStatus() + "\n Localizacion: " + e.getLocalizedMessage() + "\n Menssaje: " + e.getMessage());
-            }
-        } 
+        }
     }
 }
